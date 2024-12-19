63e7f74252be7906064c59239f9e1435fd288b8e
Sampath Surineni <sampath.surineni@gmail.com>
2024-12-19 23:37:28 +0530

    added interval to load images

--- src/refreshInterval/refreshInterval.jsx
+++ src/refreshInterval/refreshInterval.jsx
@@ -1,7 +1,20 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 
 export function RefreshInterval() {
     const [data, setData] = useState(null);
+    const [trigger, setTrigger] = useState(0);
+    const fetchDataInterval = useRef();
+    const setFetchDataInterval = (value) => {
+        if (fetchDataInterval.current) {
+            clearInterval(fetchDataInterval.current);
+            fetchDataInterval.current = null;
+        }
+        if (value > 0) {
+            fetchDataInterval.current = setInterval(() => {
+                setTrigger(Date.now());
+            }, value);
+        }
+    }
     useEffect(() => {
         fetch('https://random.dog/woof.json')
             .then(async res => {
@@ -10,10 +23,19 @@ export function RefreshInterval() {
                 setData((response))
             })
             .catch(err => console.console.warn(err));
-    }, []);
+    }, [trigger]);
     return (
         <>
             <h1>Refresh Interval</h1>
+            <div>
+                <select defaultValue="0" onChange={(e) => setFetchDataInterval(e.target.value)}>
+                    <option value="0">Auto Refresh: OFF</option>
+                    <option value="5000">5 seconds</option>
+                    <option value="15000">15 seconds</option>
+                    <option value="30000">30 seconds</option >
+                    <option value="60000">1 minute</option>
+                </select >
+            </div >
             <img className="dog-image" src={data && data.url} alt="dog image" />
         </>
     )
