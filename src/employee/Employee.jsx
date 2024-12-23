5c0c69c6b133afe35b085ccc88ba9ca1389d547a
sampath surineni <sampath.surineni@gmail.com>
2024-12-23 16:10:49 +0530

    showing employee list

--- src/employee/Employee.jsx
+++ src/employee/Employee.jsx
@@ -1,10 +1,13 @@
 import EmployeeControls from "../employeeControls/employeeControls";
 import EmployeeList from "../employeeList/employeeList";
 export default function Employee() {
+    const employeeData = [ { id: 1, name: "Alice", title: "Software Engineer" }, 
+        { id: 2, name: "Bob", title: "Product Manager" }, 
+        { id: 3, name: "Charlie", title: "UX Designer"}];
     return (
         <>
             <EmployeeControls />
-            <EmployeeList />
+            <EmployeeList employeeData={employeeData}/>
         </>
     )
 }
--- src/employeeList/employeeList.jsx
+++ src/employeeList/employeeList.jsx
@@ -1,7 +1,34 @@
-export default function EmployeeList() {
+import './styles.css'
+import PropTypes from 'prop-types'
+export default function EmployeeList({ employeeData }) {
     return (
         <>
             <h1>Employee List</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employeeData.map((employee) => (
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.name}</td>
+                            <td>{employee.title}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
+EmployeeList.propTypes = {
+    employeeData: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    })).isRequired,
+};
