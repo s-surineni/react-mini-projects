b62dcd028df247c242ce81b5f475a372e9fe20f3
sampath surineni <sampath.surineni@gmail.com>
2025-01-02 23:06:00 +0530

    able to delete multiple rows from table

--- src/employee/Employee.jsx
+++ src/employee/Employee.jsx
@@ -1,7 +1,7 @@
 import EmployeeControls from "../employeeControls/employeeControls";
 import EmployeeList from "../employeeList/employeeList";
 import { useState } from "react";
-const employeeData = [{ id: 1, name: "Alice", title: "Software Engineer" },
+let employeeData = [{ id: 1, name: "Alice", title: "Software Engineer" },
     { id: 2, name: "Bob", title: "Product Manager" },
     { id: 3, name: "Charlie", title: "UX Designer" }];
     
@@ -11,10 +11,13 @@ export default function Employee() {
     const [checkboxState, setCheckboxState] = useState(new Array(employeeData.length).fill(false));
     const deleteSelected = () => {
         const newCheckBoxState = [...checkboxState];
-        const filteredCheckBoxes = newCheckBoxState.filter((ele, idx) =>  {
-            if (ele) {
-                employeeData.splice(idx, 1)
-            }
+        employeeData = employeeData.filter((ele, idx) => {
+           return !newCheckBoxState[idx]
+        })
+        const filteredCheckBoxes = newCheckBoxState.filter((ele) =>  {
+            // if (ele) {
+            //     employeeData.splice(idx, 1)
+            // }
             return !ele;
         })
         setCheckboxState(filteredCheckBoxes)
