29786017237ed463d63b88a283c838de7e6221bb
sampath surineni <sampath.surineni@gmail.com>
2025-03-30 13:07:03 +0530

    added animation to progress bar

--- src/ProgressBarSample/ProgressBarSample.jsx
+++ src/ProgressBarSample/ProgressBarSample.jsx
@@ -1,7 +1,12 @@
 import ProgressBar from "../ProgressBar/ProgressBar"
+import { useEffect, useState } from "react";
 
 const ProgressBarSample = () => {
-    return <ProgressBar completed={50}></ProgressBar>
+    const [completed, setCompleted] = useState(0);
+    useEffect(() => {
+        setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+    }, []);
+    return <ProgressBar completed={completed}></ProgressBar>
 }
 
 export default ProgressBarSample;
