3333a1d0e8093b4ee47d651ee3be58872f6a737a
sampath surineni <sampath.surineni@gmail.com>
2025-05-10 16:03:35 +0530

    showing children in indented way

--- src/FileTree/FileTree.jsx
+++ src/FileTree/FileTree.jsx
@@ -1,10 +1,16 @@
+import './FileTree.css';
+
 export default function FileTree({ data }) {
     const rootItems = data.filter(item => !item.parentId);
 
     const renderItem = (item) => {
+        const children = data.filter(child => child.parentId === item.id);
         return (
             <div key={item.id}>
                 {item.name}
+                <div className="children">
+                    {children.map(child => renderItem(child))}
+                </div>
             </div>
         )
     }
